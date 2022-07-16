7b8499a295bc0f44c394655383e044c6d7292ac1
Ronald <eronald@web.de>
2022-07-16 09:44:46 +0200

    Added args and split up stage 1&2

--- train_mvcnn.py
+++ train_mvcnn.py
@@ -2,6 +2,12 @@ import argparse
 import json
 import os
 import wandb
+import sys
+
+# Only for windows
+# Set Max Split size because it keeps crashing
+if os.name == 'nt':
+    os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:21"
 
 import torch
 import torch.nn as nn
@@ -33,6 +39,9 @@ parser.add_argument(
 parser.add_argument(
     "-val_path", type=str, default="data/modelnet40_images_new_12x/*/test"
 )
+parser.add_argument("-stage", type=int, default=1)
+parser.add_argument("-svcnn_name", type=str, default="")
+parser.add_argument("-freeze", type=bool, default=True)
 parser.set_defaults(train=False)
 
 
@@ -53,135 +62,164 @@ if __name__ == "__main__":
     project_name = 'r-convnext-modelnet'
 
     args = parser.parse_args()
-    wandb.init(
-        project=project_name,
-        entity="icheler-team",
-        name=f"{args.name}_stage_1",
-        tags=["stage1"],
-        config={
-            "name": args.name,
-            "num_models": args.num_models,
-            "lr": args.lr,
-            "weight_decay": args.weight_decay,
-            "no_pretraining": args.no_pretraining,
-            "cnn_name": args.cnn_name,
-            "num_views": args.num_views,
-        },
-    )
-
-    pretraining = not args.no_pretraining
-    log_dir = f"runs/{args.name}"
-    create_folder(log_dir)
-    config_f = open(os.path.join(log_dir, "config.json"), "w")
-    json.dump(vars(args), config_f)
-    config_f.close()
-
-    # STAGE 1
-    log_dir = f"runs/{args.name}/stage_1"
-    create_folder(log_dir)
-    cnet = SVCNN(
-        args.name, nclasses=40, pretraining=pretraining, cnn_name=args.cnn_name
-    )
-
-    optimizer = optim.Adam(
-        cnet.parameters(), lr=args.lr, weight_decay=args.weight_decay
-    )
 
     n_models_train = args.num_models * args.num_views
 
-    train_dataset = SingleImgDataset(
-        args.train_path,
-        scale_aug=False,
-        rot_aug=False,
-        num_models=n_models_train,
-        num_views=args.num_views,
-    )
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=64, shuffle=True, num_workers=0
-    )
+    if not torch.cuda.is_available():
+        print("Not using cuda... exiting")
+        sys.exit()
 
-    val_dataset = SingleImgDataset(
-        args.val_path, scale_aug=False, rot_aug=False, test_mode=True
-    )
-    val_loader = torch.utils.data.DataLoader(
-        val_dataset, batch_size=64, shuffle=False, num_workers=0
-    )
-    print("num_train_files: " + str(len(train_dataset.filepaths)))
-    print("num_val_files: " + str(len(val_dataset.filepaths)))
-    trainer = ModelNetTrainer(
-        cnet,
-        train_loader,
-        val_loader,
-        optimizer,
-        nn.CrossEntropyLoss(),
-        "svcnn",
-        log_dir,
-        num_views=1,
-    )
-    # TODO trainer.train(30)
-    trainer.train(1)
-    wandb.finish()
+    if args.stage == 1:
+        wandb.init(
+            project=project_name,
+            entity="icheler-team",
+            name=f"{args.name}_stage_1",
+            tags=["stage1"],
+            config={
+                "name": args.name,
+                "num_models": args.num_models,
+                "lr": args.lr,
+                "batch_size": args.batchSize,
+                "weight_decay": args.weight_decay,
+                "no_pretraining": args.no_pretraining,
+                "cnn_name": args.cnn_name,
+                "num_views": args.num_views,
+            },
+        )
 
-    # STAGE 2
+        pretraining = not args.no_pretraining
+        log_dir = f"runs/{args.name}"
+        create_folder(log_dir)
+        config_f = open(os.path.join(log_dir, "config.json"), "w")
+        json.dump(vars(args), config_f)
+        config_f.close()
 
-    wandb.init(
-        project=project_name,
-        entity="icheler-team",
-        name=f"{args.name}_stage_2",
-        tags=["stage2"],
-        config={
-            "name": args.name,
-            "num_models": args.num_models,
-            "lr": args.lr,
-            "weight_decay": args.weight_decay,
-            "no_pretraining": args.no_pretraining,
-            "cnn_name": args.cnn_name,
-            "num_views": args.num_views,
-        },
-    )
-    log_dir = f"runs/{args.name}/stage_2"
-    create_folder(log_dir)
-    cnet_2 = MVCNN(
-        args.name, cnet, nclasses=40, cnn_name=args.cnn_name, num_views=args.num_views
-    )
-    del cnet
+        # STAGE 1
+        log_dir = f"runs/{args.name}/stage_1"
+        create_folder(log_dir)
+        cnet = SVCNN(
+            args.name, nclasses=40, pretraining=pretraining, cnn_name=args.cnn_name
+        )
 
-    optimizer = optim.Adam(
-        cnet_2.parameters(),
-        lr=args.lr,
-        weight_decay=args.weight_decay,
-        betas=(0.9, 0.999),
-    )
+        optimizer = optim.Adam(
+            cnet.parameters(), lr=args.lr, weight_decay=args.weight_decay
+        )
 
-    train_dataset = MultiviewImgDataset(
-        args.train_path,
-        scale_aug=False,
-        rot_aug=False,
-        num_models=n_models_train,
-        num_views=args.num_views,
-    )
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=args.batchSize, shuffle=False, num_workers=0
-    )  # shuffle needs to be false! it's done within the trainer
+        train_dataset = SingleImgDataset(
+            args.train_path,
+            scale_aug=False,
+            rot_aug=False,
+            num_models=n_models_train,
+            num_views=args.num_views,
+        )
+        train_loader = torch.utils.data.DataLoader(
+            train_dataset, batch_size=args.batchSize, shuffle=True, num_workers=4
+        )
 
-    val_dataset = MultiviewImgDataset(
-        args.val_path, scale_aug=False, rot_aug=False, num_views=args.num_views
-    )
-    val_loader = torch.utils.data.DataLoader(
-        val_dataset, batch_size=args.batchSize, shuffle=False, num_workers=0
-    )
-    print("num_train_files: " + str(len(train_dataset.filepaths)))
-    print("num_val_files: " + str(len(val_dataset.filepaths)))
-    trainer = ModelNetTrainer(
-        cnet_2,
-        train_loader,
-        val_loader,
-        optimizer,
-        nn.CrossEntropyLoss(),
-        "mvcnn",
-        log_dir,
-        num_views=args.num_views,
-    )
-    # TODO trainer.train(30)
-    trainer.train(1)
-    wandb.finish()
+        val_dataset = SingleImgDataset(
+            args.val_path, scale_aug=False, rot_aug=False, test_mode=True
+        )
+        val_loader = torch.utils.data.DataLoader(
+            val_dataset, batch_size=args.batchSize, shuffle=False, num_workers=4
+        )
+        print("num_train_files: " + str(len(train_dataset.filepaths)))
+        print("num_val_files: " + str(len(val_dataset.filepaths)))
+        trainer = ModelNetTrainer(
+            cnet,
+            train_loader,
+            val_loader,
+            optimizer,
+            nn.CrossEntropyLoss(),
+            "svcnn",
+            log_dir,
+            num_views=1,
+        )
+        # TODO trainer.train(30)
+        trainer.train(1)
+        wandb.finish()
+
+    elif args.stage == 2:
+        if args.svcnn_name == "":
+            print("SVCNN Model name required")
+            sys.exit()
+
+        cnet = SVCNN(
+            args.svcnn_name, nclasses=40, pretraining=False, cnn_name=args.cnn_name
+        )
+
+        cnet = cnet.load(f"runs/{args.svcnn_name}/stage_1")
+
+        # STAGE 2
+
+        wandb.init(
+            project=project_name,
+            entity="icheler-team",
+            name=f"{args.name}_stage_2",
+            tags=["stage2"],
+            config={
+                "name": args.name,
+                "num_models": args.num_models,
+                "lr": args.lr,
+                "batch_size": args.batchSize,
+                "weight_decay": args.weight_decay,
+                "no_pretraining": args.no_pretraining,
+                "cnn_name": args.cnn_name,
+                "num_views": args.num_views,
+            },
+        )
+        log_dir = f"runs/{args.name}/stage_2"
+        create_folder(log_dir)
+        cnet_2 = MVCNN(
+            args.name,
+            cnet,
+            nclasses=40,
+            cnn_name=args.cnn_name,
+            num_views=args.num_views
+        )
+
+        del cnet
+
+        # Freeze first part of net to improve training time / enable transfer learning
+        if args.freeze:
+            for param in cnet_2.net_1.parameters():
+                param.requires_grad(False)
+
+        optimizer = optim.Adam(
+            cnet_2.parameters(),
+            lr=args.lr,
+            weight_decay=args.weight_decay,
+            betas=(0.9, 0.999),
+        )
+
+        train_dataset = MultiviewImgDataset(
+            args.train_path,
+            scale_aug=False,
+            rot_aug=False,
+            num_models=n_models_train,
+            num_views=args.num_views,
+        )
+        train_loader = torch.utils.data.DataLoader(
+            train_dataset, batch_size=args.batchSize, shuffle=False, num_workers=4
+        )  # shuffle needs to be false! it's done within the trainer
+
+        val_dataset = MultiviewImgDataset(
+            args.val_path, scale_aug=False, rot_aug=False, num_views=args.num_views
+        )
+        val_loader = torch.utils.data.DataLoader(
+            val_dataset, batch_size=args.batchSize, shuffle=False, num_workers=4
+        )
+        print("num_train_files: " + str(len(train_dataset.filepaths)))
+        print("num_val_files: " + str(len(val_dataset.filepaths)))
+        trainer = ModelNetTrainer(
+            cnet_2,
+            train_loader,
+            val_loader,
+            optimizer,
+            nn.CrossEntropyLoss(),
+            "mvcnn",
+            log_dir,
+            num_views=args.num_views,
+        )
+        # TODO trainer.train(30)
+        trainer.train(1)
+        wandb.finish()
